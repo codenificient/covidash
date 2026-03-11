b700cce8a8a18ae9165c13a8cc8ed133cb00974a
CodenificienT <61930093+codenificient@users.noreply.github.com>
2026-03-11 00:24:15 -0400

    Change proxy target for analytics API

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     "/api/analytics",
     createProxyMiddleware({
-      target: "https://analytics-dashboard-phi-six.vercel.app",
+      target: "https://codenalytics.vercel.app",
       changeOrigin: true,
       pathRewrite: { "^/api/analytics": "/api" },
       logLevel: "debug",
